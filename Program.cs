1cda28f1ffee9e4acce73aad6707c95e96e309a0
its julii <nanji828@gmail.com>
2023-01-13 14:55:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Baskettball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int taksa = int.Parse(Console.ReadLine());
+
+            double kecove = taksa - (taksa * 0.40);
+            double ekip = kecove - (kecove * 0.20);
+            double topka = ekip / 4;
+            double aksesoari = topka / 5;
+
+            double total = kecove + ekip + topka + aksesoari + taksa;
+            Console.WriteLine(total);
+
+            
+            
+        }
+    }
+}
